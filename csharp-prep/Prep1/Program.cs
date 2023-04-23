e3aebdbc30c2c7334ac299592445e956d7a91378
Fernando Gonzalez Rolandi <gon20016@byui.edu>
2023-04-22 23:43:31 -0300

    Prep1 and Prep2 are modified

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your fisrt name?");
+        string fisrtName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {fisrtName} {lastName}.");
+        
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90 && percent <=100)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80 && percent <=89)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70 && percent <=79)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60 && percent <=69)
+        {
+            letter = "D";
+        }
+        else if (percent >= 0 && percent <=59)
+        {
+        letter = "F";
+        }
+        else
+        {
+            letter = "unknown";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70 && percent <= 100)
+        {
+            Console.WriteLine("You passed!");
+        }
+         else if (percent < 0 || percent > 100)
+        {
+            Console.WriteLine("Enter a valid percent value from 1 to 100");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
